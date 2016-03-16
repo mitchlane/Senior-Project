804f795e6f7eae7ed15787ed146371bb7f3c4bf9
mitchlane <mitchlane44@gmail.com>
2016-03-16 00:49:58 -0700

    trying 10 variables

--- main.cpp
+++ main.cpp
@@ -13,93 +13,21 @@ typedef Eigen::Vector2d Gradient;
 typedef DScalar1<double, Gradient> DScalar;
 
 using namespace std;
-void printEllipse(unsigned int points, double a0, double b0)
-{
-  cout << endl <<"x" << "\t" << "y" << "\t" << "dx/da" << "\t" << "dy/db" << endl;
-  cout << "----" << "\t" << "----" << "\t" << "----" << "\t" << "----" << endl;
-  double slice = 2 * PI / points;
-  DiffScalarBase::setVariableCount(2);
-  Eigen::Vector2d ab(a0, b0);
-  
-  DScalar as(0, ab[0]);
-  DScalar bs(0, ab[1]);
-  
-  for(int i = 0; i <= points; ++i)
-  {
-    double angle = slice * i;
-    DScalar x = (as * cos(angle));
-    DScalar y = (bs * sin(angle));
-    
-    std::cout << x << std::endl;
-    std::cout << y << std::endl << std::endl;
-    
-/*
-    cout << round(x.getValue() * 1000.0) / 1000.0
-         << "\t" << round(y.getValue() * 1000.0) / 1000.0
-         << "\t" << x.getGradient() 
-         << "\t" << y.getGradient() << endl;
-*/
-  }
-}
-
 int main(int argc, char** argv)
 {
-  unsigned int resolution;
-  double xRad;
-  double yRad;
+  DiffScalarBase::setVariableCount(10);
+  Eigen::VectorXd vars(10);
+  printf("here\n");
+  DScalar x[10];
   
-  cout << "Enter the resolution of the ellipse." << endl;
-  cin >> resolution;
-  cout << "Enter the x radius." << endl;
-  cin >> xRad;
-  cout << "Enter the y radius." << endl;
-  cin >> yRad;
+  for(int i = 0; i <= 9; ++i)
+  {
+    vars[i] = i;
+    x[i] = {0, vars[i]};
+  }
   
-  printEllipse(resolution, xRad, yRad);
-
+  DScalar test = x[0] + x[1] + x[2] + x[3] + x[4] + x[5]
+                 + x[6] + x[7] + x[8] + x[9];
+  
   return 0;
 }
-
-/*
-Enter the resolution of the ellipse.
-10
-Enter the x radius.
-6
-Enter the y radius.
-8
-
-x	y	dx/da	dy/db
-----	----	----	----
-[6, grad=[1;  0]]
-[0, grad=[0;  0]]
-
-[4.8541, grad=[0.809;  0]]
-[4.70228, grad=[0.5878;  0]]
-
-[1.8541, grad=[0.309;  0]]
-[7.60845, grad=[0.9511;  0]]
-
-[-1.8541, grad=[-0.309;  -0]]
-[7.60845, grad=[0.9511;  0]]
-
-[-4.8541, grad=[-0.809;  -0]]
-[4.70228, grad=[0.5878;  0]]
-
-[-6, grad=[-1;  -0]]
-[7.4705e-13, grad=[9.338e-14;  0]]
-
-[-4.8541, grad=[-0.809;  -0]]
-[-4.70228, grad=[-0.5878;  -0]]
-
-[-1.8541, grad=[-0.309;  -0]]
-[-7.60845, grad=[-0.9511;  -0]]
-
-[1.8541, grad=[0.309;  0]]
-[-7.60845, grad=[-0.9511;  -0]]
-
-[4.8541, grad=[0.809;  0]]
-[-4.70228, grad=[-0.5878;  -0]]
-
-[6, grad=[1;  0]]
-[-1.4941e-12, grad=[-1.868e-13;  -0]]
-*/
