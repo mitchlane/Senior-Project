b50aaf6dfb7980f0ca94052dc51f117f5ae645ef
mitchlane <mitchlane44@gmail.com>
2016-03-16 01:00:46 -0700

    tried instantiating 10 individual DScalar objects, doesn't work still

--- main.cpp
+++ main.cpp
@@ -17,17 +17,16 @@ int main(int argc, char** argv)
 {
   DiffScalarBase::setVariableCount(10);
   Eigen::VectorXd vars(10);
-  printf("here\n");
-  DScalar x[10];
   
   for(int i = 0; i <= 9; ++i)
   {
     vars[i] = i;
-    x[i] = {0, vars[i]};
   }
   
-  DScalar test = x[0] + x[1] + x[2] + x[3] + x[4] + x[5]
-                 + x[6] + x[7] + x[8] + x[9];
+  DScalar a(0, vars[0]),b(0, vars[1]),c(0, vars[2]),d(0, vars[3]),e(0, vars[4]),f(0, vars[5]),g(0, vars[6]),h(0, vars[7]),i(0, vars[8]),j(0, vars[9]);
+  
+//  DScalar test = x[0] + x[1] + x[2] + x[3] + x[4] + x[5]
+//                 + x[6] + x[7] + x[8] + x[9];
   
   return 0;
 }
