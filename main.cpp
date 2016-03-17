61239c2dbba6d8b45a93660611b50c183d545181
mitchlane <mitchlane44@gmail.com>
2016-03-16 19:23:59 -0700

    working with 10 variables

--- main.cpp
+++ main.cpp
@@ -6,27 +6,32 @@
 #include <Eigen/Cholesky>
 #include "autodiff.h"
 
-#define PI 3.1415926535897
-
 DECLARE_DIFFSCALAR_BASE();
-typedef Eigen::Vector2d Gradient;
+typedef Eigen::VectorXd Gradient;
 typedef DScalar1<double, Gradient> DScalar;
 
 using namespace std;
 int main(int argc, char** argv)
 {
-  DiffScalarBase::setVariableCount(10);
   Eigen::VectorXd vars(10);
+  DiffScalarBase::setVariableCount(10);
   
-  for(int i = 0; i <= 9; ++i)
+  for(int i = 0; i <= 1; ++i)
   {
     vars[i] = i;
   }
   
-  DScalar a(0, vars[0]),b(0, vars[1]),c(0, vars[2]),d(0, vars[3]),e(0, vars[4]),f(0, vars[5]),g(0, vars[6]),h(0, vars[7]),i(0, vars[8]),j(0, vars[9]);
+//  DScalar a(0, vars[0]);
+//  DScalar b(1, vars[1]);
+//  DScalar c(2, vars[2]);
+//  DScalar c(0, vars[2]);
   
-//  DScalar test = x[0] + x[1] + x[2] + x[3] + x[4] + x[5]
-//                 + x[6] + x[7] + x[8] + x[9];
+  DScalar a(0, vars[0]),b(1, vars[1]),c(2, vars[2]),d(3, vars[3]),e(4, vars[4]),f(5, vars[5]),g(6, vars[6]),h(7, vars[7]),i(8, vars[8]),j(9, vars[9]);
+  
+  //DScalar test = x[0] + x[1] + x[2] + x[3] + x[4] + x[5]
+  //               + x[6] + x[7] + x[8] + x[9];
+  DScalar test = a + b + c + d + e + f + g + h + i + j - 2;
+  std::cout << test << std::endl;
   
   return 0;
 }
