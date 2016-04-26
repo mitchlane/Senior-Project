22ecb52b61f3c3ba849614e8994a124e53940360
mitchlane <mitchlane44@gmail.com>
2016-04-25 18:06:24 -0700

    added comments

--- main.cpp
+++ main.cpp
@@ -16,22 +16,23 @@ int main(int argc, char** argv)
   Eigen::VectorXd vars(10);
   DiffScalarBase::setVariableCount(10);
   
-  for(int i = 0; i <= 1; ++i)
+  for(int i = 0; i <= 9; ++i)
   {
     vars[i] = i;
   }
   
-//  DScalar a(0, vars[0]);
-//  DScalar b(1, vars[1]);
-//  DScalar c(2, vars[2]);
-//  DScalar c(0, vars[2]);
+  DScalar a(0, vars[0]),b(1, vars[1]),c(2, vars[2]),d(3, vars[3]),
+    e(4, vars[4]),f(5, vars[5]),g(6, vars[6]),h(7, vars[7]),i(8, vars[8]),
+    j(9, vars[9]);
   
-  DScalar a(0, vars[0]),b(1, vars[1]),c(2, vars[2]),d(3, vars[3]),e(4, vars[4]),f(5, vars[5]),g(6, vars[6]),h(7, vars[7]),i(8, vars[8]),j(9, vars[9]);
-  
-  //DScalar test = x[0] + x[1] + x[2] + x[3] + x[4] + x[5]
-  //               + x[6] + x[7] + x[8] + x[9];
-  DScalar test = a + b + c + d + e + f + g + h + i + j - 2;
+              // 0 + 2*1 + 3*2 + 4*3 + 5*4 + 6*5 + 7*6 + 8*7 + 9*8 + 0*9
+  DScalar test = a + 2*b + 3*c + 4*d + 5*e + 6*f + 7*g + 8*h + 9*i + 0*j;
   std::cout << test << std::endl;
   
   return 0;
 }
+
+
+/*
+ * [240, grad=[1;  2;  3;  4;  5;  6;  7;  8;  9;  0]]
+*/
